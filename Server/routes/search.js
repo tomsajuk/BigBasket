2cb0301514319d9ab62f2d4647a456a13e6151b8
Thomas Saju Koshy <tomsajuk@gmail.com>
2017-09-09 22:33:00 +0530

    added shelfs

--- Server/routes/search.js
+++ Server/routes/search.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 
+var app = express();
+
 var url = "mongodb://localhost:27017/Basket";
 
 router.get('/code/:barcode', function(req, res, next) {
@@ -11,14 +13,18 @@ router.get('/code/:barcode', function(req, res, next) {
 	  if (err) throw err;
 	  db.collection("Inventory").find({Barcode: code }, { _id: false}).toArray( function(err, result) {
 	    if (err) throw err;
+
 	    console.log(result);
 	    res.json(result);
-	    db.close();
 	    res.status(200);
 	    res.end();
+	    db.close();
 	  });
 	});
 });
+
+
+
 
 router.get('/name/:name', function(req, res, next) {
 	var product_name = req.params.name;
@@ -27,16 +33,26 @@ router.get('/name/:name', function(req, res, next) {
 	  db.collection("Inventory").find({"Product Name":{$regex: product_name, $options:"$i" }}, { _id: false}).toArray( function(err, result) {
 	    if (err) throw err;
 	    console.log(result);
-	    db.close();
 
 	    if(result.length == 0) {
 	    	res.status(202);
 	    }
 	    else {
-		    res.json(result);
-		    res.status(200);
+
+	    	var obj = [{"Inventory":result}];
+		    var category = result[0].Category;
+
+		    db.collection("Shelf").find({Category: category }, { _id: false}).toArray( function(err, result) {
+			    if (err) throw err;
+			    console.log(result);
+			    obj.push({"Shelf":result});
+			    console.log(obj[0].Inventory[0].Category);
+			    res.json(obj);
+			    db.close();
+			    res.status(200);
+			    res.end();
+			  });
 		}
-		res.end();
 		
 	  });
 	});
